18127a973eefe8e79403d41fe5387f43c28c6314
Mohsen <33067800+nicro23@users.noreply.github.com>
2026-04-12 21:04:42 +0300

    Update message forwarding to multiple target channels

--- forwarder.py
+++ forwarder.py
@@ -4,18 +4,24 @@ import os
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
-
-source_channel = os.getenv("SOURCE_CHANNEL")
-target_channel = os.getenv("TARGET_CHANNEL")
+TARGET_CHANNELS = [
+    -1001310984791,
+    -1002036270701,
+    -1001134494888,
+    -1001458480018,
+]
+# source_channel = os.getenv("SOURCE_CHANNEL")
+# target_channel = os.getenv("TARGET_CHANNEL")
 
 client = TelegramClient('session', api_id, api_hash)
 
-@client.on(events.NewMessage(chats=source_channel))
+@client.on(events.NewMessage(chats=TARGET_CHANNELS))
 async def handler(event):
-    await client.forward_messages(target_channel, event.message)
+    await client.forward_messages(channel, event.message)
 
 async def main():
     await client.start()
+    channel = await client.get_entity(-1003731587014)
     print("Bot is running...")
     await client.run_until_disconnected()
 
